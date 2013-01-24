d5bef6c772967c8afef523a0a914417e437b570f
Will Farrington <wfarr@github.com>
2013-01-24 12:07:48 -0800

    fix md5

--- files/brews/mongodb.rb
+++ files/brews/mongodb.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Mongodb < Formula
   homepage 'http://www.mongodb.org/'
   url 'http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-2.2.2.tgz'
-  md5 'b3808eeb6fe481f87db176cd3ab31119f94f7cc1'
+  md5 'f0c3b8039a8900c5d1b51343035ec7f5'
   version '2.2.2-boxen1'
 
   skip_clean :all
