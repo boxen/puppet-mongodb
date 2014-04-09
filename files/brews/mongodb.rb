f47e417e4b60cdad63fca49df0f8bbc9494e8eb7
Mark Tareshawty <tarebyte@gmail.com>
2014-04-09 09:42:02 -0400

    update mongo to 2.6.0

--- files/brews/mongodb.rb
+++ files/brews/mongodb.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Mongodb < Formula
   homepage 'http://www.mongodb.org/'
-  url 'http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-2.4.4.tgz'
-  sha1 'd9abc5c3aa6e7c6c29bc7b4a15028091931ec7bb'
-  version '2.4.4-boxen1'
+  url 'http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-2.6.0.tgz'
+  sha1 '35f8efe61d992f4b71c9205a9dbcab50e745c9a3'
+  version '2.6.0-boxen1'
 
   skip_clean :all
 
