a250b074a55afb5617d14d2a3c76f125accea487
Mark Tareshawty <tarebyte@gmail.com>
2014-04-11 15:50:52 -0400

    removed plist, template does the same thing

--- files/brews/mongodb.rb
+++ files/brews/mongodb.rb
@@ -71,46 +71,6 @@ class Mongodb < Formula
     EOS
   end
 
-  plist_options :manual => "mongod --config #{HOMEBREW_PREFIX}/etc/mongod.conf"
-
-  def plist; <<-EOS.undent
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/mongod</string>
-        <string>--config</string>
-        <string>#{etc}/mongod.conf</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <false/>
-      <key>WorkingDirectory</key>
-      <string>#{HOMEBREW_PREFIX}</string>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/mongodb/output.log</string>
-      <key>StandardOutPath</key>
-      <string>#{var}/log/mongodb/output.log</string>
-      <key>HardResourceLimits</key>
-      <dict>
-        <key>NumberOfFiles</key>
-        <integer>1024</integer>
-      </dict>
-      <key>SoftResourceLimits</key>
-      <dict>
-        <key>NumberOfFiles</key>
-        <integer>1024</integer>
-      </dict>
-    </dict>
-    </plist>
-    EOS
-  end
-
   test do
     system "#{bin}/mongod", '--sysinfo'
   end
